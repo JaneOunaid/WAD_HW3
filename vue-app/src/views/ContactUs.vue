76815a62514ea87eea216f40f597c36eca92f0e4
JaneOunaid <jane.ounaid@outlook.com>
2021-11-21 16:57:49 +0200

    Hw task 4

--- vue-app/src/views/ContactUs.vue
+++ vue-app/src/views/ContactUs.vue
@@ -1,5 +1,51 @@
 <template>
   <div class="contacus">
-    <h1>This is a Contact Us page</h1>
+    <h1>Contact Us</h1>
+    <h3>Marketing</h3>
+    <ul>
+      <li>
+        <p>
+          Telephone number: 1234567
+        </p>
+      </li>
+      <li>
+        <p>
+          Email: marketing@postit.com
+        </p>
+      </li>
+      <li>
+        <p>
+          Team leader: Mary
+        </p>
+      </li>
+      <li>
+        <p>
+          Team: John, Joanne, Harry
+        </p>
+      </li>
+    </ul>
+    <h3>Customer support</h3>
+    <ul>
+      <li>
+        <p>
+          Telephone number: 2234567
+        </p>
+      </li>
+      <li>
+        <p>
+          Email: support@postit.com
+        </p>
+      </li>
+      <li>
+        <p>
+          Team leader: Steve
+        </p>
+      </li>
+      <li>
+        <p>
+          Team: Lisa, Kristen
+        </p>
+      </li>
+    </ul>
   </div>
 </template>
